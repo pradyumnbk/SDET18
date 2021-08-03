ba1951f794860d9a0a2a9d9d81eed5e05a56d2e2
pradyumnbk <barik.pradyumn0@gmail.com>
2021-08-04 02:27:49 +0530

    my first commit in sdet18

---
+++ demomaven/src/test/java/com/Ty/RMG/demomaven/CreateProjectTest.java
@@ -0,0 +1,13 @@
+package com.Ty.RMG.demomaven;
+
+import org.testng.annotations.Test;
+
+public class CreateProjectTest 
+{
+	@Test
+	public void createProjectTest()
+	{
+		System.out.println("you are in create project module");
+	}
+
+}
