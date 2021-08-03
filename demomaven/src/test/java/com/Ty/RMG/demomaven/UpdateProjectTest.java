4f31f05cb7cd92cc596272f7eeb092b2caeae213
pradyumnbk <barik.pradyumn0@gmail.com>
2021-08-04 02:37:45 +0530

    my edited commit

---
+++ demomaven/src/test/java/com/Ty/RMG/demomaven/UpdateProjectTest.java
@@ -0,0 +1,12 @@
+package com.Ty.RMG.demomaven;
+
+import org.testng.annotations.Test;
+
+public class UpdateProjectTest 
+{
+	@Test
+	public void updateProjectTest()
+	{
+		System.out.println("you are in update project module");
+	}
+}
